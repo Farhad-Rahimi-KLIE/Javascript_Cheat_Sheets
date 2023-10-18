222b005e6ee8c567edb31ea81313a4a461c29e88
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:29:26 +0430

    Add files via upload

---
+++ Window Properties.js
@@ -0,0 +1,56 @@
+closed — Checks whether a window has been closed or not and returns 
+true or false
+
+defaultStatus — Sets or returns the default text in the statusbar of 
+a window
+
+document — Returns the document object for the window
+
+frames — Returns all <iframe> elements in the current window.
+
+history — Provides the History object for the window
+
+innerHeight — The inner height of a window’s content area
+
+innerWidth — The inner width of the content area
+
+length — Find out the number of <iframe> elements in the window
+
+location — Returns the location object for the window
+
+name — Sets or returns the name of a window
+
+navigator — Returns the Navigator object for the window
+
+opener — Returns a reference to the window that created the window
+
+outerHeight — The outer height of a window, including toolbars/
+scrollbars 
+
+outerWidth — The outer width of a window, including toolbars/
+scrollbars
+
+pageXOffset — Number of pixels the current document has been scrolled 
+horizontally
+
+pageYOffset — Number of pixels the document has been scrolled 
+vertically
+
+parent — The parent window of the current window
+
+screen — Returns the Screen object for the window
+
+screenLeft — The horizontal coordinate of the window (relative to 
+screen)
+
+screenTop — The vertical coordinate of the window
+
+screenX — Same as screenLeft but needed for some browsers
+
+screenY — Same as screenTop but needed for some browsers
+
+self — Returns the current window
+
+status — Sets or returns the text in the statusbar of a window
+
+top — Returns the topmost browser window

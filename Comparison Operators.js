2ace914217a7e282198a29348017b8515bf91ba2
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:11:45 +0430

    Add files via upload

---
+++ Comparison Operators.js
@@ -0,0 +1,9 @@
+== — Equal to
+=== — Equal value and equal type
+!= — Not equal
+!== — Not equal value or not equal type
+> — Greater than
+< — Less than
+>= — Greater than or equal to
+<= — Less than or equal to
+? — Ternary operator

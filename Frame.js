6959620dd092bd20e99d60e4a6cc5dfcf0ab9d47
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:16:38 +0430

    Add files via upload

---
+++ Frame.js
@@ -0,0 +1,20 @@
+onabort — The loading of a media is aborted.
+
+onbeforeunload — Event occurs before the document is about to be 
+unloaded 
+
+onerror — An error occurs while loading an external file
+
+onhashchange — There have been changes to the anchor part of a URL
+
+onload — When an object has loaded
+
+onpagehide — The user navigates away from a webpage
+
+onpageshow — When the user navigates to a webpage
+
+onresize — The document view is resized
+
+onscroll — An element’s scrollbar is being scrolled
+
+onunload — Event occurs when a page has unloaded

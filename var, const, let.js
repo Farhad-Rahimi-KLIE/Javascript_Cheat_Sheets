76c195bc35910608cda491d1b6fa08de28b5dc45
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:28:52 +0430

    Add files via upload

---
+++ var, const, let.js
@@ -0,0 +1,9 @@
+var — The most common variable. Can be reassigned but only accessed 
+within a function. Variables defined with var move to the top when 
+code is executed.
+
+const — Can not be reassigned and not accessible before they appear 
+within the code.
+
+let — Similar to const, however, let variable can be reassigned but 
+not re-declared.

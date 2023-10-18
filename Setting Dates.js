48f4681f35ee90a9e500cad01eaa49595ab70d42
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:26:57 +0430

    Add files via upload

---
+++ Setting Dates.js
@@ -0,0 +1,6 @@
+Date() — Creates a new date object with the current date and time
+Date(2017, 5, 21, 3, 23, 10, 0) — Create a custom date object. The 
+numbers represent year, month, day, hour, minutes, seconds, 
+milliseconds. You can omit anything you want except for year and 
+month.
+Date("2017-06-23") — Date declaration as a string

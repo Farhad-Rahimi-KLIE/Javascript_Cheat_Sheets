315a97604d9ae320bdd596ee8e7a3638237d801f
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:15:58 +0430

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,23 @@
+onblur — When an element loses focus
+
+onchange — The content of a form element changes 
+(for <input>, <select>and <textarea>)
+
+onfocus — An element gets focus
+
+onfocusin — When an element is about to get focus
+
+onfocusout — The element is about to lose focus
+
+oninput — User input on an element
+
+oninvalid — An element is invalid
+
+onreset — A form is reset
+
+onsearch — The user writes something in a search field 
+(for <input="search">)
+
+onselect — The user selects some text (for <input> and <textarea>)
+
+onsubmit — A form is submitted

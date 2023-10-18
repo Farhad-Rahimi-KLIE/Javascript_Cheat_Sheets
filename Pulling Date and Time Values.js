e91a76055e95c42d8d60a0b6ffcf16f4c2fa5dec
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:25:24 +0430

    Add files via upload

---
+++ Pulling Date and Time Values.js
@@ -0,0 +1,14 @@
+getDate() — Get the day of the month as a number (1-31)
+getDay() — The weekday as a number (0-6)
+getFullYear() — Year as a four digit number (yyyy)
+getHours() — Get the hour (0-23)
+getMilliseconds() — The millisecond (0-999)
+getMinutes() — Get the minute (0-59)
+getMonth() — Month as a number (0-11)
+getSeconds() — Get the second (0-59)
+getTime() — Get the milliseconds since January 1, 1970
+getUTCDate() — The day (date) of the month in the specified date 
+according to universal time (also available for day, month, fullyear, 
+hours, minutes etc.)
+parse — Parses a string representation of a date, and returns the 
+number of milliseconds since January 1, 1970

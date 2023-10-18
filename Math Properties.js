d4319499c6638b4488e84d327c0cb9fbda34bc59
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:22:27 +0430

    Add files via upload

---
+++ Math Properties.js
@@ -0,0 +1,8 @@
+E — Euler’s number
+LN2 — The natural logarithm of 2
+LN10 — Natural logarithm of 10
+LOG2E — Base 2 logarithm of E
+LOG10E — Base 10 logarithm of E
+PI — The number PI
+SQRT1_2 — Square root of 1/2
+SQRT2 — The square root of 2

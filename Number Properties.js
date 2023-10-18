d034f5720bd1ca84d0650e88d33fa2901acc8419
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:24:15 +0430

    Add files via upload

---
+++ Number Properties.js
@@ -0,0 +1,6 @@
+MAX_VALUE — The maximum numeric value representable in JavaScript
+MIN_VALUE — Smallest positive numeric value representable in 
+JavaScript
+NaN — The “Not-a-Number” value
+NEGATIVE_INFINITY — The negative Infinity value
+POSITIVE_INFINITY — Positive Infinity value

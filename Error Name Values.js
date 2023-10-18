62a65cfb4d7545c0b7f795dad229f898d4ca84f6
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:15:01 +0430

    Add files via upload

---
+++ Error Name Values.js
@@ -0,0 +1,15 @@
+name — Sets or returns the error name
+
+message — Sets or returns an error message in string from
+
+EvalError — An error has occurred in the eval() function
+
+RangeError — A number is “out of range”
+
+ReferenceError — An illegal reference has occurred
+
+SyntaxError — A syntax error has occurred
+
+TypeError — A type error has occurred
+
+URIError — An encodeURI() error has occurred

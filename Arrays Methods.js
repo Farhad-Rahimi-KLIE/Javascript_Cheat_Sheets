97bdf3ece908d343dd39dafa0891c4ba6e356edb
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:10:16 +0430

    Array method was uplaoded

---
+++ Arrays Methods.js
@@ -0,0 +1,30 @@
+concat() — Join several arrays into one
+
+indexOf() — Returns the primitive value of the specified object
+
+join() — Combine elements of an array into a single string and return 
+the string
+
+lastIndexOf() — Gives the last position at which a given element 
+appears in an array
+
+pop() — Removes the last element of an array
+
+push() — Add a new element at the end
+
+reverse() — Sort elements in descending order
+
+shift() — Remove the first element of an array
+
+slice() — Pulls a copy of a portion of an array into a new array
+
+sort() — Sorts elements alphabetically
+
+splice() — Adds elements in a specified way and position
+
+toString() — Converts elements to strings
+
+unshift() — Adds a new element to the beginning
+
+valueOf() — Returns the first position at which a given element 
+appears in an array

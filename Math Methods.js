8af1bcff7d49b9d377af61d0a34fe7f24a5d68f7
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:21:42 +0430

    Add files via upload

---
+++ Math Methods.js
@@ -0,0 +1,18 @@
+abs(x) — Returns the absolute (positive) value of x
+acos(x) — The arccosine of x, in radians
+asin(x) — Arcsine of x, in radians
+atan(x) — The arctangent of x as a numeric value
+atan2(y,x) — Arctangent of the quotient of its arguments
+ceil(x) — Value of x rounded up to its nearest integer
+cos(x) — The cosine of x (x is in radians)
+exp(x) — Value of Ex
+floor(x) — The value of x rounded down to its nearest integer
+log(x) — The natural logarithm (base E) of x
+max(x,y,z,...,n) — Returns the number with the highest value
+min(x,y,z,...,n) — Same for the number with the lowest value
+pow(x,y) — X to the power of y
+random() — Returns a random number between 0 and 1
+round(x) — The value of x rounded to its nearest integer
+sin(x) — The sine of x (x is in radians)
+sqrt(x) — Square root of x
+tan(x) — The tangent of an angle

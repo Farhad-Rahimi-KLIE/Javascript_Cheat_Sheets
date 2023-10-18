98f1563a30ea6ac50340f3fba296820660604f60
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:14:12 +0430

    Add files via upload

---
+++ Element Methods.js
@@ -0,0 +1,41 @@
+getAttribute() — Returns the specified attribute value of an element 
+node
+
+getAttributeNS() — Returns string value of the attribute with the 
+specified namespace and name
+
+getAttributeNode() — Gets the specified attribute node
+
+getAttributeNodeNS() — Returns the attribute node for the attribute 
+with the given namespace and name
+
+getElementsByTagName() — Provides a collection of all child elements 
+with the specified tag name
+
+getElementsByTagNameNS() — Returns a live HTMLCollection of elements 
+with a certain tag name belonging to the given namespace
+
+hasAttribute() — Returns true if an element has any attributes, 
+otherwise false
+
+hasAttributeNS() — Provides a true/false value indicating whether the 
+current element in a given namespace has the specified attribute
+
+removeAttribute() — Removes a specified attribute from an element
+
+removeAttributeNS() — Removes the specified attribute from an element 
+within a certain namespace
+
+removeAttributeNode() — Takes away a specified attribute node and 
+returns the removed node
+
+setAttribute() — Sets or changes the specified attribute to a 
+specified value
+
+setAttributeNS() — Adds a new attribute or changes the value of an 
+attribute with the given namespace and name
+
+setAttributeNode() — Sets or changes the specified attribute node
+
+setAttributeNodeNS() — Adds a new namespaced attribute node to an 
+element

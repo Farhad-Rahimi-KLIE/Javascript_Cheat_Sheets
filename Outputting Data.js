0e4293d38e09579408c3e75e99ea13a64db5677d
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:24:50 +0430

    Add files via upload

---
+++ Outputting Data.js
@@ -0,0 +1,11 @@
+alert() — Output data in an alert box in the browser window
+
+confirm() — Opens up a yes/no dialog and returns true/false depending 
+on user click
+
+console.log() — Writes information to the browser console, good for 
+debugging purposes
+
+document.write() — Write directly to the HTML document
+
+prompt() — Creates an dialogue for user input

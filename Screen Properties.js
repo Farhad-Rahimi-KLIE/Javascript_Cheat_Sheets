47be76b493bb376d9bf3b4c92c1b78244e2fc5e6
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:25:56 +0430

    Add files via upload

---
+++ Screen Properties.js
@@ -0,0 +1,14 @@
+availHeight — Returns the height of the screen (excluding the Windows 
+Taskbar)
+
+availWidth — Returns the width of the screen (excluding the Windows 
+Taskbar)
+
+colorDepth — Returns the bit depth of the color palette for 
+displaying images
+
+height — The total height of the screen
+
+pixelDepth — The color resolution of the screen in bits per pixel
+
+width — The total width of the screen

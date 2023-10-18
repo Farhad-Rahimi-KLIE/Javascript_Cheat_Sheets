5953ea9434194cfa81d3e93eec19963ef47bf981
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:26:11 +0430

    Add files via upload

---
+++ Quantifiers.js
@@ -0,0 +1,10 @@
+n+ — Matches any string that contains at least one n
+n* — Any string that contains zero or more occurrences of n
+n? — A string that contains zero or one occurrences of n
+n{X} — String that contains a sequence of X n’s
+n{X,Y} — Strings that contains a sequence of X to Y n’s
+n{X,} — Matches any string that contains a sequence of at least X n’s
+n$ — Any string with n at the end of it
+^n — String with n at the beginning of it
+?=n — Any string that is followed by a specific string n
+?!n — String that is not followed by a specific string n

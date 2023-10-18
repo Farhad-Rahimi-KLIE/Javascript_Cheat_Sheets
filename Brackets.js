3d74c126c9f77ed815b540801bb17c985f2fe087
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:10:33 +0430

    Add files via upload

---
+++ Brackets.js
@@ -0,0 +1,5 @@
+[abc] — Find any of the characters between the brackets
+[^abc] — Find any character not in the brackets
+[0-9] — Used to find any digit from 0 to 9
+[A-z] — Find any character from uppercase A to lowercase z
+(a|b|c) — Find any of the alternatives separated with |

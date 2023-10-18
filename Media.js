9b9ce2bed895e39bce4779256611fcd96543d730
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:22:12 +0430

    Add files via upload

---
+++ Media.js
@@ -0,0 +1,49 @@
+onabort — Media loading is aborted
+
+oncanplay — The browser can start playing media (e.g. a file has 
+buffered enough)
+
+oncanplaythrough — When browser can play through media without 
+stopping
+
+ondurationchange — The duration of the media changes
+
+onended — The media has reach its end
+
+onerror — Happens when an error occurs while loading an external file
+
+onloadeddata — Media data is loaded
+
+onloadedmetadata — Meta data (like dimensions and duration) are 
+loaded
+
+onloadstart — Browser starts looking for specified media
+
+onpause — Media is paused either by the user or automatically
+
+onplay — The media has been started or is no longer paused
+
+onplaying — Media is playing after having been paused or stopped for 
+buffering
+
+onprogress — Browser is in the process of downloading the media
+
+onratechange — The playing speed of the media changes
+
+onseeked — User is finished moving/skipping to a new position in the 
+media
+
+onseeking — The user starts moving/skipping
+
+onstalled — The browser is trying to load the media but it is not 
+available
+
+onsuspend — Browser is intentionally not loading media
+
+ontimeupdate — The playing position has changed (e.g. because of fast 
+forward)
+
+onvolumechange — Media volume has changed (including mute)
+
+onwaiting — Media paused but expected to resume (for example, 
+buffering)

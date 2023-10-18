94d7ba813b791a1b8a1a19c8b3e3538a8d864116
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:23:38 +0430

    Add files via upload

---
+++ Node Properties.js
@@ -0,0 +1,28 @@
+attributes — Returns a live collection of all attributes registered 
+to and element
+
+baseURI — Provides the absolute base URL of an HTML element
+
+childNodes — Gives a collection of an element’s child nodes
+
+firstChild — Returns the first child node of an element
+
+lastChild — The last child node of an element
+
+nextSibling — Gives you the next node at the same node tree level
+
+nodeName — Returns the name of a node
+
+nodeType — Returns the type of a node
+
+nodeValue — Sets or returns the value of a node
+
+ownerDocument — The top-level document object for this node
+
+parentNode — Returns the parent node of an element
+
+previousSibling — Returns the node immediately preceding the current 
+one
+
+textContent — Sets or returns the textual content of a node and its 
+descendants

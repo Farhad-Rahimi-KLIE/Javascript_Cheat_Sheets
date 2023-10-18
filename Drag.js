f0f9211a15519c9d994ff5f6f43d6e2f278a8f23
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:14:28 +0430

    Add files via upload

---
+++ Drag.js
@@ -0,0 +1,13 @@
+ondrag — An element is dragged.
+
+ondragend — The user has finished dragging the element
+
+ondragenter — The dragged element enters a drop target
+
+ondragleave — A dragged element leaves the drop target
+
+ondragover — The dragged element is on top of the drop target
+
+ondragstart — User starts to drag an element
+
+ondrop — Dragged element is dropped on the drop target

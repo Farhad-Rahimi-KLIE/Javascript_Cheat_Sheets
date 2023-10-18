581d536845a0c4490556f5cefd91c523e384b1ad
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:27:51 +0430

    Add files via upload

---
+++ String Methods.js
@@ -0,0 +1,41 @@
+charAt() — Returns a character at a specified position inside a 
+string 
+
+charCodeAt() — Gives you the unicode of character at that position
+
+concat() — Concatenates (joins) two or more strings into one
+
+fromCharCode() — Returns a string created from the specified sequence 
+of UTF-16 code units
+
+indexOf() — Provides the position of the first occurrence of a 
+specified text within a string
+
+lastIndexOf() — Same as indexOf() but with the last occurrence, 
+searching backwards
+
+match() — Retrieves the matches of a string against a search pattern
+
+replace() — Find and replace specified text in a string
+
+search() — Executes a search for a matching text and returns its 
+position 
+
+slice() — Extracts a section of a string and returns it as a new 
+string
+
+split() — Splits a string object into an array of strings at a 
+specified position
+
+substr() — Similar to slice() but extracts a substring depended on a 
+specified number of characters
+
+substring() — Also similar to slice() but can’t accept negative 
+indices
+
+toLowerCase() — Convert strings to lower case
+
+toUpperCase() — Convert strings to upper case
+
+valueOf() — Returns the primitive value (that has no properties or 
+methods) of a string object

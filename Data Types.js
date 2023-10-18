3f7fa517d2a726858062fdbc9d142902fcc464d9
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:13:16 +0430

    Add files via upload

---
+++ Data Types.js
@@ -0,0 +1,7 @@
+Numbers — var age = 23 
+Variables — var x 
+Text (strings) — var a = "init" 
+Operations — var b = 1 + 2 + 3
+True or fase statements — var c = true 
+Constant numbers — const PI = 3.14 
+Objects — var name = {firstName:"John", lastName:”Doe"}

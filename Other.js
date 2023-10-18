cdc37411cb6bf512cb2be11b171ea713d5e8534e
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:25:06 +0430

    Add files via upload

---
+++ Other.js
@@ -0,0 +1,26 @@
+transitionend — Fired when a CSS transition has completed
+
+onmessage — A message is received through the event source
+
+onoffline — Browser starts to work offline
+
+ononline — The browser starts to work online
+
+onpopstate — When the window’s history changes
+
+onshow — A <menu> element is shown as a context menu
+
+onstorage — A Web Storage area is updated
+
+ontoggle — The user opens or closes the <details> element
+
+onwheel — Mouse wheel rolls up or down over an element
+
+ontouchcancel — Screen touch is interrupted
+
+ontouchend — User finger is removed from a touch screen
+
+ontouchmove — A finger is dragged across the screen
+
+ontouchstart — Finger is placed on touch screen
+

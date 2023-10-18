7116e19f5cab330e5ab2ef6735386e30fe7f6ad9
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:23:21 +0430

    Add files via upload

---
+++ Number Methods.js
@@ -0,0 +1,11 @@
+toExponential() — Returns a string with a rounded number written as 
+exponential notation
+
+toFixed() — Returns the string of a number with a specified number of 
+decimals
+
+toPrecision() — String of a number written with a specified length
+
+toString() — Returns a number as a string
+
+valueOf() — Returns a number as a number

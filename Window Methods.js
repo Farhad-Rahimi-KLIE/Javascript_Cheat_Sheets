bf7bf3d7c16ab3344192641ba05b00bdbe8f2c56
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:29:41 +0430

    Add files via upload

---
+++ Window Methods.js
@@ -0,0 +1,40 @@
+alert() — Displays an alert box with a message and an OK button
+
+blur() — Removes focus from the current window
+
+clearInterval() — Clears a timer set with setInterval()
+
+clearTimeout() — Clears a timer set with setTimeout()
+
+close() — Closes the current window
+
+confirm() — Displays a dialogue box with a message and 
+an OK and Cancelbutton
+
+focus() — Sets focus to the current window
+
+moveBy() — Moves a window relative to its current position
+
+moveTo() — Moves a window to a specified position
+
+open() — Opens a new browser window
+
+print() — Prints the content of the current window
+
+prompt() — Displays a dialogue box that prompts the visitor for input
+
+resizeBy() — Resizes the window by the specified number of pixels
+
+resizeTo() — Resizes the window to a specified width and height
+
+scrollBy() — Scrolls the document by a specified number of pixels
+
+scrollTo() — Scrolls the document to specified coordinates
+
+setInterval() — Calls a function or evaluates an expression at 
+specified intervals
+
+setTimeout() — Calls a function or evaluates an expression after a 
+specified interval
+
+stop() — Stops the window from loading

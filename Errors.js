6d88355ce51960aa2b9ea5bee9b3c51b7e244334
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:15:43 +0430

    Add files via upload

---
+++ Errors.js
@@ -0,0 +1,9 @@
+try — Lets you define a block of code to test for errors
+
+catch — Set up a block of code to execute in case of an error
+
+throw — Create custom error messages instead of the standard 
+JavaScript errors
+
+finally — Lets you execute code, after try and catch, regardless of 
+the result

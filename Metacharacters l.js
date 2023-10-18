501549774da32250ea21fdd3e89a743fb41e9036
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:23:03 +0430

    Add files via upload

---
+++ Metacharacters l.js
@@ -0,0 +1,16 @@
+. — Find a single character, except newline or line terminator
+\w — Word character
+\W — Non-word character
+\d — A digit
+\D — A non-digit character
+\s — Whitespace character
+\S — Non-whitespace character
+\b — Find a match at the beginning/end of a word
+\B — A match not at the beginning/end of a word
+\0 — NUL character
+\n — A new line character
+\f — Form feed character
+\r — Carriage return character
+\t — Tab character
+\v — Vertical tab character
+\xxx — The character specified by an octal number xxx

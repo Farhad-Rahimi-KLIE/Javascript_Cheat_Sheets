5f99d75c44d43b78ce97d374a4f243142ae98da6
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:27:13 +0430

    Add files via upload

---
+++ Set Part of a Date.js
@@ -0,0 +1,11 @@
+setDate() — Set the day as a number (1-31)
+setFullYear() — Sets the year (optionally month and day)
+setHours() — Set the hour (0-23)
+setMilliseconds() — Set milliseconds (0-999)
+setMinutes() — Sets the minutes (0-59)
+setMonth() — Set the month (0-11)
+setSeconds() — Sets the seconds (0-59)
+setTime() — Set the time (milliseconds since January 1, 1970)
+setUTCDate() — Sets the day of the month for a specified date 
+according to universal time (also available for day, month, fullyear, 
+hours, minutes etc.)

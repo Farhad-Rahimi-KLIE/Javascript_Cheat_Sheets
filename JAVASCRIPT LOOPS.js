299a1daa2b59b12d9afc30c52fc520b3961a2242
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:20:41 +0430

    Add files via upload

---
+++ JAVASCRIPT LOOPS.js
@@ -0,0 +1,14 @@
+for (before loop; condition for loop; execute after loop) { 
+ // what to do during the loop 
+}
+for — The most common way to create a loop in JavaScript
+
+while — Sets up conditions under which aloop executes
+
+do while — Similar to the while loop, however, it executes at least 
+once and performs a check at the end to see if the condition is met 
+to execute again
+
+break — Used to stop and exit the cycle at certain conditions
+
+continue — Skip parts of the cycle if certain conditions are met

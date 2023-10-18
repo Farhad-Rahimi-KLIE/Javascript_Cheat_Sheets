4f97d06deaebbcd9770f7e0056fe5bdd060e95fc
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:17:14 +0430

    Add files via upload

---
+++ Global Functions.js
@@ -0,0 +1,19 @@
+decodeURI() — Decodes a Uniform Resource Identifier (URI) created 
+by encodeURI or similar
+
+decodeURIComponent() — Decodes a URI component
+
+encodeURI() — Encodes a URI into UTF-8
+
+encodeURIComponent() — Same but for URI components
+eval() — Evaluates JavaScript code represented as a string
+
+isFinite() — Determines whether a passed value is a finite number
+
+isNaN() — Determines whether a value is NaN or not
+
+Number() — Returns a number converted from its argument
+
+parseFloat() — Parses an argument and returns a floating point number
+
+parseInt() — Parses its argument and returns an integer

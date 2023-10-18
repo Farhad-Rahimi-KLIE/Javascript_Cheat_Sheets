05ed704c2414bb3b1a0e0cd0cb96541aa1c4f06c
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-19 04:22:45 +0430

    Add files via upload

---
+++ Mouse.js
@@ -0,0 +1,22 @@
+onclick — The event occurs when the user clicks on an element.
+
+oncontextmenu — User right-clicks on an element to open a context 
+menu
+
+ondblclick — The user double-clicks on an element
+
+onmousedown — User presses a mouse button over an element
+
+onmouseenter — The pointer moves onto an element
+
+onmouseleave — Pointer moves out of an element
+
+onmousemove — The pointer is moving while it is over an element
+
+onmouseover — When the pointer is moved onto an element or one of its 
+children
+
+onmouseout — User moves the mouse pointer out of an element or one of 
+its children
+
+onmouseup — The user releases a mouse button while over an element

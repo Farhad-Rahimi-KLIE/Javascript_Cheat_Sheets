5e61e2ae2d1e5bcd126e3ecb681afb1360d384dd
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 16:38:59 +0430

    Update Select Modify DOM Element Attributes.js

--- Select Modify DOM Element Attributes.js
+++ Select Modify DOM Element Attributes.js
@@ -1,5 +1,5 @@
 document.getElementById("elementId”).innerText;
-DOM.innerHTML = "<p>welcome home, boss</p>";
+DOM.innerHTML = "<p>welcome Home, boss</p>";
 DOM.className = "class-one class-two";
 DOM.id = "element-id";
 DOM.setAttribute("id"

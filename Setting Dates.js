f9f4cd8b5aec823b748f95c2cc01b276dd9ed5dd
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 16:39:29 +0430

    Update Setting Dates.js

--- Setting Dates.js
+++ Setting Dates.js
@@ -1,4 +1,4 @@
-Date() — Creates a new date object with the current date and time
+Date() — Creates a new Date object with the current date and time
 Date(2017, 5, 21, 3, 23, 10, 0) — Create a custom date object. The 
 numbers represent year, month, day, hour, minutes, seconds, 
 milliseconds. You can omit anything you want except for year and 

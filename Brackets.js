bba9f72e418f4299957a972d8f7c6986b16f633c
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 16:26:31 +0430

    Update Brackets.js

--- Brackets.js
+++ Brackets.js
@@ -1,4 +1,4 @@
-[abc] — Find any of the characters between the brackets
+[abc] — Find any of the Characters between the brackets
 [^abc] — Find any character not in the brackets
 [0-9] — Used to find any digit from 0 to 9
 [A-z] — Find any character from uppercase A to lowercase z

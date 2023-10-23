5a60f19694e11310daaef3b0c8aa8577560c64e2
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-23 05:01:44 +0430

    Update Mouse.js

--- Mouse.js
+++ Mouse.js
@@ -1,4 +1,4 @@
-// javascript Tutorials 
+// javascript Tutorials
 onclick — The Event occurs when the user clicks on an element.
 
 oncontextmenu — User right-clicks on an element to open a context 
